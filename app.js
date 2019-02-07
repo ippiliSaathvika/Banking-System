0624d6025acf4d2a635768e9fd4aebb1d76c9990
Ippili Saathvika <saathvikaippili@gmail.com>
2019-02-07 21:16:37 +0530

    Working

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express'),
-	app = express(),
-	path = __dirname + '/views/',
+const express = require('express'),
 	bodyParser = require('body-parser'),
-	router = express.Router(),
 	cookieSession = require('cookie-session'),
-	mongoose = require('mongoose');
+	path = __dirname + '/views/',
+	mongodb = require('mongodb'),
+	uri = 'mongodb://saathvika:saathvika13@ds039261.mlab.com:39261/online_banking',
+	app = express();
 app.use(
 	cookieSession({
 		name: 'session',
-		keys: ['ahms'],
+		keys: ['banking'],
 		// Cookie Options
 		maxAge: 24 * 60 * 60 * 1000 // 24 hours
 	})
@@ -23,28 +23,54 @@ app.set('port', process.env.PORT || 8000);
 app.use(express.static(__dirname + '/public'));
 app.set('views', path);
 app.set('view engine', 'ejs');
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/', (req, res) => {
+	res.render('about');
+});
 
-mongoose.connect('mongodb://saathvika:l@ughy123@ds039261.mlab.com:39261/online_banking');//database name online_banking
- 
-var Schema = new mongoose.Schema({
-	_id    : String,
-	name: String,
-	age   : Number
-});
- 
-var user = mongoose.model('emp', Schema);
- 
-app.post('/new', function(req, res){
-	new user({
-		_id    : req.body.email,
-		name: req.body.name,
-		age   : req.body.age				
-	}).save(function(err, doc){
-		if(err) res.json(err);
-		else    res.send('Successfully inserted!');
+app.get('/login', (req, res) => {
+	res.render('index1');
+})
+
+app.post('/onLogin', (req, res) => {
+	console.log(req.body.email);
+	let userData = 
+		{
+			email: req.body.email,
+			name: req.body.name,
+			age: req.body.age
+		};
+	mongodb.MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+		if (err) throw err;
+		let db = client.db('online_banking');
+		let users = db.collection('users');
+		users.insertOne(userData, function (err, result) {
+			if (err) throw err;
+			res.send('Done');
+		});
 	});
-});
+})
+
+// mongoose.connect('mongodb://saathvika:l@ughy123@ds039261.mlab.com:39261/online_banking');//database name online_banking
+
+// var Schema = new mongoose.Schema({
+// 	_id: String,
+// 	name: String,
+// 	age: Number
+// });
+
+// var user = mongoose.model('emp', Schema);
+
+// app.post('/new', function (req, res) {
+// 	new user({
+// 		_id: req.body.email,
+// 		name: req.body.name,
+// 		age: req.body.age
+// 	}).save(function (err, doc) {
+// 		if (err) res.json(err);
+// 		else res.send('Successfully inserted!');
+// 	});
+// });
